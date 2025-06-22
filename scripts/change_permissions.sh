bfebafa4d8fbcd998ce9940c9cfbd96a27f03dda
Chaohao Zhu <chaohaozhu@Mac-mini.local>
2025-06-21 21:35:36 -0400

    clean up unnecessary scripts, finally passed

--- scripts/change_permissions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-# Give execute permissions to all .sh files in the scripts directory
-chmod +x /home/ec2-user/app/scripts/*.sh
--- scripts/stop_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-# Use the full, absolute path to pm2
-/home/ec2-user/.nvm/versions/node/v20.19.2/bin/pm2 stop app || true
-/home/ec2-user/.nvm/versions/node/v20.19.2/bin/pm2 delete app || true
